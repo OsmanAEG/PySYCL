bec570653a7bf146e9b74f1c3659226e42cbe622
Osman El-Ghotmi <osman.elghotmi@gmail.com>
2023-05-17 10:55:10 -0400

    trying to fix PySYCL pages

--- docs/_sources/index.rst.txt
+++ docs/_sources/index.rst.txt
@@ -1,5 +1,5 @@
 .. PySYCL documentation master file, created by
-   sphinx-quickstart on Wed May 17 06:22:21 2023.
+   sphinx-quickstart on Wed May 17 10:50:33 2023.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
--- sphinx/conf.py
+++ sphinx/conf.py
@@ -14,13 +14,11 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../build/PySYCL'))
 
-
 # -- Project information -----------------------------------------------------
 
 project = 'PySYCL'
 copyright = '2023, Osman El-Ghotmi'
 author = 'Osman El-Ghotmi'
-
 
 # -- General configuration ---------------------------------------------------
 
@@ -36,15 +34,6 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
-html_logo = "images/PySYCL Logo.png"
 
 html_theme_options = {
   'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
@@ -62,6 +51,14 @@ html_theme_options = {
   'includehidden': True,
   'titles_only': False
 }
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+html_logo = "images/PySYCL Logo.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
