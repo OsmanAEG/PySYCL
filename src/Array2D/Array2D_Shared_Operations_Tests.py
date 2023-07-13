119b180652a3de21ac049875c28ba60ea480a31f
Osman El-Ghotmi <osman.elghotmi@gmail.com>
2023-07-13 07:35:17 -0400

    updates

--- src/Array2D/Array2D_Shared_Operations_Tests.py
+++ src/Array2D/Array2D_Shared_Operations_Tests.py
@@ -1,25 +1,37 @@
 import sys
 sys.path.insert(0, '../build')
 import unittest
-import random
+import math
 import numpy as np
-from Helper_Functions_Array2D import *
 
 from pysycl import device
 from pysycl import array2D
 
-class TestArray2D_Element_Wise_Operations_Shared(unittest.TestCase):
+class TestArray2D_Operations(unittest.TestCase):
   """
   Test Array2D Operations
   """
+  def array2D_shared_init(self, rows, cols, Q):
+    """
+    Test Array2D init
+    :param rows: The number of rows in the array.
+    :param cols: The number of columns in the array.
+    :param Q: The initial value of the array.
 
-  ###########################################################
-  ###########################################################
-  ###########################################################
-  # Tests for add_el_Array2D (Shared) addition
+    :type rows: int
+    :type cols: int
+    :type Q: float
 
-  def test_add_el_array2D(self):
-    print("\nTESTING THE ELEMENT-WISE ADDITION OF TWO ARRAY2D SHARED OBJECTS")
+    :returns: None
+    """
+    A = array2D.array2D_shared(rows, cols, Q)
+    return A
+
+  def add_array2D_test_loop(self, arr2D_1, arr2D_2, np_1, np_2):
+    
+
+  def test_add_array2D(self):
+    print("\nTESTING THE ADDITION OF TWO ARRAY2D SHARED OBJECTS")
     """
     Test add_Array2D for Array2D_Shared Objects
     :returns: None
@@ -31,332 +43,10 @@ class TestArray2D_Element_Wise_Operations_Shared(unittest.TestCase):
     Q = device.device_object(0, 0)
 
     ###########################################################
-    # Tests for add_el_Array2D (Shared) addition
-
-    # These tests should pass
-    ###########################################################
+    # Tests for add_Array2D (Shared) addition
     try:
       M = 100
       N = 50
-
-      A_arr2D, B_arr2D, A_np, B_np = create_4_arrays_shared(M, N, Q)
-
-      element_fill(A_arr2D, B_arr2D, A_np, B_np, M, N)
-
-      C_arr2D = array2D.add_el_array2D(A_arr2D, B_arr2D)
-      C_np = A_np + B_np
-
-      Q.wait()
-      get_C_arr2D = C_arr2D.get_data()
-      check_elements_equal(get_C_arr2D, C_np, M, N)
-    except:
-      raise AssertionError("add_el_Array2D failed to add two Array2D_Shared objects.")
-
-    ###########################################################
-    try:
-      M = 393
-      N = 528
-
-      A_arr2D, B_arr2D, A_np, B_np = create_4_arrays_shared(M, N, Q)
-
-      element_fill(A_arr2D, B_arr2D, A_np, B_np, M, N)
-
-      C_arr2D = array2D.add_el_array2D(A_arr2D, B_arr2D)
-      C_np = A_np + B_np
-
-      Q.wait()
-      get_C_arr2D = C_arr2D.get_data()
-      check_elements_equal(get_C_arr2D, C_np, M, N)
-    except:
-      raise AssertionError("add_el_Array2D failed to add two Array2D_Shared objects.")
-
-    ###########################################################
-    try:
-      M = 25
-      N = 25
-
-      A_arr2D, B_arr2D, A_np, B_np = create_4_arrays_shared(M, N, Q)
-
-      element_fill(A_arr2D, B_arr2D, A_np, B_np, M, N)
-
-      C_arr2D = array2D.add_el_array2D(A_arr2D, B_arr2D)
-      C_np = A_np + B_np
-
-      Q.wait()
-      get_C_arr2D = C_arr2D.get_data()
-      check_elements_equal(get_C_arr2D, C_np, M, N)
-    except:
-      raise AssertionError("add_el_Array2D failed to add two Array2D_Shared objects.")
-
-    # These tests should fail
-    ###########################################################
-    with self.assertRaises(RuntimeError):
-      A_arr2D = array2D_shared_init(500, 200, Q)
-      B_arr2D = array2D_shared_init(500, 201, Q)
-      C_arr2D = array2D.add_el_array2D(A_arr2D, B_arr2D)
-
-    with self.assertRaises(RuntimeError):
-      A_arr2D = array2D_shared_init(400, 200, Q)
-      B_arr2D = array2D_shared_init(500, 200, Q)
-      C_arr2D = array2D.add_el_array2D(A_arr2D, B_arr2D)
-
-  ###########################################################
-  ###########################################################
-  ###########################################################
-  # Tests for subtract_el_Array2D (Shared) subtraction
-
-  def test_subtract_el_array2D(self):
-    print("\nTESTING THE ELEMENT-WISE SUBTRACTION OF TWO ARRAY2D SHARED OBJECTS")
-    """
-    Test subtract_el_Array2D for Array2D_Shared Objects
-    :returns: None
-    :raises: AssertionError
-    """
-
-    ###########################################################
-    # Device for testing
-    Q = device.device_object(0, 0)
-
-    ###########################################################
-    # Tests for subtract_el_Array2D (Shared) subtraction
-
-    # These tests should pass
-    ###########################################################
-    try:
-      M = 100
-      N = 50
-
-      A_arr2D, B_arr2D, A_np, B_np = create_4_arrays_shared(M, N, Q)
-
-      element_fill(A_arr2D, B_arr2D, A_np, B_np, M, N)
-
-      C_arr2D = array2D.subtract_el_array2D(A_arr2D, B_arr2D)
-      C_np = A_np - B_np
-
-      Q.wait()
-      get_C_arr2D = C_arr2D.get_data()
-      check_elements_equal(get_C_arr2D, C_np, M, N)
-    except:
-      raise AssertionError("subtract_el_Array2D failed to subtract two Array2D_Shared objects.")
-
-    ###########################################################
-    try:
-      M = 393
-      N = 528
-
-      A_arr2D, B_arr2D, A_np, B_np = create_4_arrays_shared(M, N, Q)
-
-      element_fill(A_arr2D, B_arr2D, A_np, B_np, M, N)
-
-      C_arr2D = array2D.subtract_el_array2D(A_arr2D, B_arr2D)
-      C_np = A_np - B_np
-
-      Q.wait()
-      get_C_arr2D = C_arr2D.get_data()
-      check_elements_equal(get_C_arr2D, C_np, M, N)
-    except:
-      raise AssertionError("subtract_el_Array2D failed to subtract two Array2D_Shared objects.")
-
-    ###########################################################
-    try:
-      M = 25
-      N = 25
-
-      A_arr2D, B_arr2D, A_np, B_np = create_4_arrays_shared(M, N, Q)
-
-      element_fill(A_arr2D, B_arr2D, A_np, B_np, M, N)
-
-      C_arr2D = array2D.subtract_el_array2D(A_arr2D, B_arr2D)
-      C_np = A_np - B_np
-
-      Q.wait()
-      get_C_arr2D = C_arr2D.get_data()
-      check_elements_equal(get_C_arr2D, C_np, M, N)
-    except:
-      raise AssertionError("subtract_el_Array2D failed to subtract two Array2D_Shared objects.")
-
-    # These tests should fail
-    ###########################################################
-    with self.assertRaises(RuntimeError):
-      A_arr2D = array2D_shared_init(500, 200, Q)
-      B_arr2D = array2D_shared_init(500, 201, Q)
-      C_arr2D = array2D.subtract_el_array2D(A_arr2D, B_arr2D)
-
-    with self.assertRaises(RuntimeError):
-      A_arr2D = array2D_shared_init(400, 200, Q)
-      B_arr2D = array2D_shared_init(500, 200, Q)
-      C_arr2D = array2D.subtract_el_array2D(A_arr2D, B_arr2D)
-
-  ###########################################################
-  ###########################################################
-  ###########################################################
-  # Tests for subtract_el_Array2D (Shared) addition
-
-  def test_multiply_el_array2D(self):
-    print("\nTESTING THE ELEMENT-WISE MULTIPLICATION OF TWO ARRAY2D SHARED OBJECTS")
-    """
-    Test multiply_el_Array2D for Array2D_Shared Objects
-    :returns: None
-    :raises: AssertionError
-    """
-
-    ###########################################################
-    # Device for testing
-    Q = device.device_object(0, 0)
-
-    ###########################################################
-    # Tests for multiply_el_Array2D (Shared) multiplication
-
-    # These tests should pass
-    ###########################################################
-    try:
-      M = 100
-      N = 50
-
-      A_arr2D, B_arr2D, A_np, B_np = create_4_arrays_shared(M, N, Q)
-
-      element_fill(A_arr2D, B_arr2D, A_np, B_np, M, N)
-
-      C_arr2D = array2D.multiply_el_array2D(A_arr2D, B_arr2D)
-      C_np = A_np * B_np
-
-      Q.wait()
-
-      get_C_arr2D = C_arr2D.get_data()
-      check_elements_equal(get_C_arr2D, C_np, M, N)
-    except:
-      raise AssertionError("multiply_el_Array2D failed to multiply two Array2D_Shared objects.")
-
-    ###########################################################
-    try:
-      M = 393
-      N = 528
-
-      A_arr2D, B_arr2D, A_np, B_np = create_4_arrays_shared(M, N, Q)
-
-      element_fill(A_arr2D, B_arr2D, A_np, B_np, M, N)
-
-      C_arr2D = array2D.multiply_el_array2D(A_arr2D, B_arr2D)
-      C_np = A_np * B_np
-
-      Q.wait()
-      get_C_arr2D = C_arr2D.get_data()
-      check_elements_equal(get_C_arr2D, C_np, M, N)
-    except:
-      raise AssertionError("multiply_el_Array2D failed to multiply two Array2D_Shared objects.")
-
-    ###########################################################
-    try:
-      M = 25
-      N = 25
-
-      A_arr2D, B_arr2D, A_np, B_np = create_4_arrays_shared(M, N, Q)
-
-      element_fill(A_arr2D, B_arr2D, A_np, B_np, M, N)
-
-      C_arr2D = array2D.multiply_el_array2D(A_arr2D, B_arr2D)
-      C_np = A_np * B_np
-
-      Q.wait()
-      get_C_arr2D = C_arr2D.get_data()
-      check_elements_equal(get_C_arr2D, C_np, M, N)
-    except:
-      raise AssertionError("multiply_el_Array2D failed to multiply two Array2D_Shared objects.")
-
-    # These tests should fail
-    ###########################################################
-    with self.assertRaises(RuntimeError):
-      A_arr2D = array2D_shared_init(500, 200, Q)
-      B_arr2D = array2D_shared_init(500, 201, Q)
-      C_arr2D = array2D.multiply_el_array2D(A_arr2D, B_arr2D)
-
-    with self.assertRaises(RuntimeError):
-      A_arr2D = array2D_shared_init(400, 200, Q)
-      B_arr2D = array2D_shared_init(500, 200, Q)
-      C_arr2D = array2D.multiply_el_array2D(A_arr2D, B_arr2D)
-
-  def test_divide_el_array2D(self):
-    print("\nTESTING THE ELEMENT-WISE DIVISION OF TWO ARRAY2D SHARED OBJECTS")
-    """
-    Test multiply_el_Array2D for Array2D_Shared Objects
-    :returns: None
-    :raises: AssertionError
-    """
-
-    ###########################################################
-    # Device for testing
-    Q = device.device_object(0, 0)
-
-    ###########################################################
-    # Tests for divide_el_Array2D (Shared) division
-
-    # These tests should pass
-    ###########################################################
-    try:
-      M = 5
-      N = 5
-
-      A_arr2D, B_arr2D, A_np, B_np = create_4_arrays_shared(M, N, Q)
-
-      element_fill(A_arr2D, B_arr2D, A_np, B_np, M, N)
-
-      C_arr2D = array2D.divide_el_array2D(A_arr2D, B_arr2D)
-      C_np = A_np / B_np
-
-      Q.wait()
-
-      get_C_arr2D = C_arr2D.get_data()
-      check_elements_equal(get_C_arr2D, C_np, M, N)
-    except:
-      raise AssertionError("divide_el_Array2D failed to divide two Array2D_Shared objects.")
-
-    ###########################################################
-    try:
-      M = 393
-      N = 528
-
-      A_arr2D, B_arr2D, A_np, B_np = create_4_arrays_shared(M, N, Q)
-
-      element_fill(A_arr2D, B_arr2D, A_np, B_np, M, N)
-
-      C_arr2D = array2D.divide_el_array2D(A_arr2D, B_arr2D)
-      C_np = A_np / B_np
-
-      Q.wait()
-      get_C_arr2D = C_arr2D.get_data()
-      check_elements_equal(get_C_arr2D, C_np, M, N)
-    except:
-      raise AssertionError("divide_el_Array2D failed to divide two Array2D_Shared objects.")
-
-    ###########################################################
-    try:
-      M = 25
-      N = 25
-
-      A_arr2D, B_arr2D, A_np, B_np = create_4_arrays_shared(M, N, Q)
-
-      element_fill(A_arr2D, B_arr2D, A_np, B_np, M, N)
-
-      C_arr2D = array2D.divide_el_array2D(A_arr2D, B_arr2D)
-      C_np = A_np / B_np
-
-      Q.wait()
-      get_C_arr2D = C_arr2D.get_data()
-      check_elements_equal(get_C_arr2D, C_np, M, N)
-    except:
-      raise AssertionError("divide_el_Array2D failed to divide two Array2D_Shared objects.")
-
-    # These tests should fail
-    ###########################################################
-    with self.assertRaises(RuntimeError):
-      A_arr2D = array2D_shared_init(500, 200, Q)
-      B_arr2D = array2D_shared_init(500, 201, Q)
-      C_arr2D = array2D.divide_el_array2D(A_arr2D, B_arr2D)
-
-    with self.assertRaises(RuntimeError):
-      A_arr2D = array2D_shared_init(400, 200, Q)
-      B_arr2D = array2D_shared_init(500, 200, Q)
-      C_arr2D = array2D.divide_el_array2D(A_arr2D, B_arr2D)
-
-if __name__ == '__main__':
-  unittest.main()
+      A = self.array2D_shared_init(100, 100, Q)
+      B = self.array2D_shared_init(100, 100, Q)
+      C = A.add_Array2D(B)
