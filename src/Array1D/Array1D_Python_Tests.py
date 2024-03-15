3ce01cffaaf5ce5cf93e3ec9f7edf020b3a2e0c6
Osman El-Ghotmi <osman.elghotmi@gmail.com>
2024-03-14 20:46:12 -0400

    added division test for array1d

--- src/Array1D/Array1D_Python_Tests.py
+++ src/Array1D/Array1D_Python_Tests.py
@@ -5,14 +5,26 @@ import sys
 sys.path.insert(1, '../build/')
 import pysycl
 
+print("\033[32m| ----- ARRAY 1D TEST SUITE ----- |\033[0m")
+
 ############################################
 ############## ADDITION TESTS ##############
 ############################################
 class TestArray1D_Addition(unittest.TestCase):
+  @classmethod
+  def setUpClass(cls):
+    print("\033[34mARRAY 1D TESTS: ADDITION (STARTING)\033[0m")
+
+  @classmethod
+  def tearDownClass(cls):
+    print("\033[32mARRAY 1D TESTS: ADDITION (COMPLETED)\033[0m")
+    print("\033[33m------------------------------------------\033[0m")
+
   def setUp(self):
     self.tolerance_float  = 1e-7
     self.tolerance_double = 1e-15
     self.device = pysycl.device.get_device(0, 0)
+    print("\033[33mrunning test...\033[0m")
 
   # ADDITION DOUBLE TYPE TESTS
   def test_vector_addition_double(self):
@@ -123,10 +135,20 @@ class TestArray1D_Addition(unittest.TestCase):
 ########### SUBTRACTION TESTS ##############
 ############################################
 class TestArray1D_Subtraction(unittest.TestCase):
+  @classmethod
+  def setUpClass(cls):
+    print("\033[34mARRAY 1D TESTS: SUBTRACTION (STARTING)\033[0m")
+
+  @classmethod
+  def tearDownClass(cls):
+    print("\033[32mARRAY 1D TESTS: SUBTRACTION (COMPLETED)\033[0m")
+    print("\033[33m------------------------------------------\033[0m")
+
   def setUp(self):
     self.tolerance_float  = 1e-7
     self.tolerance_double = 1e-15
     self.device = pysycl.device.get_device(0, 0)
+    print("\033[33mrunning test...\033[0m")
 
   # SUBTRACTION DOUBLE TYPE TESTS
   def test_vector_subtraction_double(self):
@@ -237,10 +259,20 @@ class TestArray1D_Subtraction(unittest.TestCase):
 ########## MULTIPLICATION TESTS ############
 ############################################
 class TestArray1D_Multiplication(unittest.TestCase):
+  @classmethod
+  def setUpClass(cls):
+    print("\033[34mARRAY 1D TESTS: MULTIPLICATION (STARTING)\033[0m")
+
+  @classmethod
+  def tearDownClass(cls):
+    print("\033[32mARRAY 1D TESTS: MULTIPLICATION (COMPLETED)\033[0m")
+    print("\033[33m------------------------------------------\033[0m")
+
   def setUp(self):
     self.tolerance_float  = 1e-7
     self.tolerance_double = 1e-15
     self.device = pysycl.device.get_device(0, 0)
+    print("\033[33mrunning test...\033[0m")
 
   # MULTIPLICATION DOUBLE TYPE TESTS
   def test_vector_multiplication_double(self):
@@ -347,5 +379,129 @@ class TestArray1D_Multiplication(unittest.TestCase):
       for i in range(N):
         self.assertEqual(A_pysycl[i], A_np[i])
 
+############################################
+############## DIVISION TESTS ##############
+############################################
+class TestArray1D_Dvision(unittest.TestCase):
+  @classmethod
+  def setUpClass(cls):
+    print("\033[34mARRAY 1D TESTS: DIVISION (STARTING)\033[0m")
+
+  @classmethod
+  def tearDownClass(cls):
+    print("\033[32mARRAY 1D TESTS: DIVISION (COMPLETED)\033[0m")
+    print("\033[33m------------------------------------------\033[0m")
+
+  def setUp(self):
+    self.tolerance_float  = 1e-7
+    self.tolerance_double = 1e-15
+    self.device = pysycl.device.get_device(0, 0)
+    print("\033[33mrunning test...\033[0m")
+
+  # DIVISION DOUBLE TYPE TESTS
+  def test_vector_division_double(self):
+    for N in [10, 100, 1000, 10000]:
+      A_pysycl = pysycl.array(N, device= self.device, dtype= pysycl.double)
+      B_pysycl = pysycl.array(N, device= self.device, dtype= pysycl.double)
+      A_pysycl.fill(86.74)
+      B_pysycl.fill(12.79)
+
+      C_pysycl = A_pysycl / B_pysycl
+      C_pysycl.mem_to_cpu()
+
+      A_np = np.full(N, 86.74, dtype= np.float64)
+      B_np = np.full(N, 12.79, dtype= np.float64)
+      C_np = A_np / B_np
+
+      for i in range(N):
+        self.assertAlmostEqual(C_pysycl[i], C_np[i], delta= self.tolerance_double)
+
+  def test_in_place_vector_division_double(self):
+    for N in [10, 100, 1000, 10000]:
+      A_pysycl = pysycl.array(N, device= self.device, dtype= pysycl.double)
+      B_pysycl = pysycl.array(N, device= self.device, dtype= pysycl.double)
+      A_pysycl.fill(86.74)
+      B_pysycl.fill(12.79)
+
+      A_pysycl /= B_pysycl
+      A_pysycl.mem_to_cpu()
+
+      A_np = np.full(N, 86.74, dtype= np.float64)
+      B_np = np.full(N, 12.79, dtype= np.float64)
+      A_np /= B_np
+
+      for i in range(N):
+        self.assertAlmostEqual(A_pysycl[i], A_np[i], delta= self.tolerance_double)
+
+  # DIVISION FLOAT TYPE TESTS
+  def test_vector_division_float(self):
+    for N in [10, 100, 1000, 10000]:
+      A_pysycl = pysycl.array(N, device= self.device, dtype= pysycl.float)
+      B_pysycl = pysycl.array(N, device= self.device, dtype= pysycl.float)
+      A_pysycl.fill(86.74)
+      B_pysycl.fill(12.79)
+
+      C_pysycl = A_pysycl / B_pysycl
+      C_pysycl.mem_to_cpu()
+
+      A_np = np.full(N, 86.74, dtype= np.float32)
+      B_np = np.full(N, 12.79, dtype= np.float32)
+      C_np = A_np / B_np
+
+      for i in range(N):
+        self.assertAlmostEqual(C_pysycl[i], C_np[i], delta= self.tolerance_float)
+
+  def test_in_place_vector_division_float(self):
+    for N in [10, 100, 1000, 10000]:
+      A_pysycl = pysycl.array(N, device= self.device, dtype= pysycl.float)
+      B_pysycl = pysycl.array(N, device= self.device, dtype= pysycl.float)
+      A_pysycl.fill(86.74)
+      B_pysycl.fill(12.79)
+
+      A_pysycl /= B_pysycl
+      A_pysycl.mem_to_cpu()
+
+      A_np = np.full(N, 86.74, dtype= np.float32)
+      B_np = np.full(N, 12.79, dtype= np.float32)
+      A_np /= B_np
+
+      for i in range(N):
+        self.assertAlmostEqual(A_pysycl[i], A_np[i], delta= self.tolerance_float)
+
+  # DIVISION INTEGER TYPE TESTS
+  def test_vector_division_int(self):
+    for N in [10, 100, 1000, 10000]:
+      A_pysycl = pysycl.array(N, device= self.device, dtype= pysycl.int)
+      B_pysycl = pysycl.array(N, device= self.device, dtype= pysycl.int)
+      A_pysycl.fill(8)
+      B_pysycl.fill(3)
+
+      C_pysycl = A_pysycl / B_pysycl
+      C_pysycl.mem_to_cpu()
+
+      A_np = np.full(N, 8, dtype= np.int32)
+      B_np = np.full(N, 3, dtype= np.int32)
+      C_np = A_np // B_np
+
+      for i in range(N):
+        self.assertEqual(C_pysycl[i], C_np[i])
+
+  def test_in_place_vector_division_int(self):
+    for N in [10, 100, 1000, 10000]:
+      A_pysycl = pysycl.array(N, device= self.device, dtype= pysycl.int)
+      B_pysycl = pysycl.array(N, device= self.device, dtype= pysycl.int)
+      A_pysycl.fill(8)
+      B_pysycl.fill(3)
+
+      A_pysycl /= B_pysycl
+      A_pysycl.mem_to_cpu()
+
+      A_np = np.full(N, 8, dtype= np.int32)
+      B_np = np.full(N, 3, dtype= np.int32)
+      A_np //= B_np
+
+      for i in range(N):
+        self.assertEqual(A_pysycl[i], A_np[i])
+
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
