e82c533cf13c204dff87bad284b1690e5a601de5
Osman El-Ghotmi <osman.elghotmi@gmail.com>
2024-03-16 17:09:21 -0400

    added more matmul tests for other data types

--- src/Linear_Algebra/Matrix_Multiplication_Python_Tests.py
+++ src/Linear_Algebra/Matrix_Multiplication_Python_Tests.py
@@ -26,7 +26,7 @@ class Device_Instance_Tests(unittest.TestCase):
     self.device = pysycl.device.get_device(0, 0)
     print("\033[33mrunning test...\033[0m")
 
-  # MATMUL
+  # MATMUL TEST DOUBLE
   def test_matmul_double(self):
     device = pysycl.device.get_device(0, 0)
 
@@ -54,6 +54,64 @@ class Device_Instance_Tests(unittest.TestCase):
     for i in range(M):
       for j in range(P):
         self.assertAlmostEqual(C_pysycl[i, j], C_np[i, j], delta= self.tolerance_double)
+
+  # MATMUL TEST FLOAT
+  def test_matmul_float(self):
+    device = pysycl.device.get_device(0, 0)
+
+    M = 65
+    N = 85
+    P = 25
+
+    A_np = np.full((M, N), 2.0, dtype=np.float32)
+    B_np = np.full((N, P), 4.0, dtype=np.float32)
+    C_np = np.full((M, P), 0.0, dtype=np.float32)
+
+    A_pysycl = pysycl.array((M, N), device= device, dtype = pysycl.float)
+    B_pysycl = pysycl.array((N, P), device= device, dtype = pysycl.float)
+    C_pysycl = pysycl.array((M, P), device= device, dtype = pysycl.float)
+
+    A_pysycl.fill(2.0)
+    B_pysycl.fill(4.0)
+    C_pysycl.fill(0.0)
+
+    C_np = np.matmul(A_np, B_np)
+    pysycl.linalg.matmul(A_pysycl, B_pysycl, C_pysycl, 32)
+
+    C_pysycl.mem_to_cpu()
+
+    for i in range(M):
+      for j in range(P):
+        self.assertAlmostEqual(C_pysycl[i, j], C_np[i, j], delta= self.tolerance_float)
+
+  # MATMUL TEST INT
+  def test_matmul_int(self):
+    device = pysycl.device.get_device(0, 0)
+
+    M = 65
+    N = 85
+    P = 25
+
+    A_np = np.full((M, N), 2, dtype=np.int32)
+    B_np = np.full((N, P), 4, dtype=np.int32)
+    C_np = np.full((M, P), 0, dtype=np.int32)
+
+    A_pysycl = pysycl.array((M, N), device= device, dtype = pysycl.int)
+    B_pysycl = pysycl.array((N, P), device= device, dtype = pysycl.int)
+    C_pysycl = pysycl.array((M, P), device= device, dtype = pysycl.int)
+
+    A_pysycl.fill(2)
+    B_pysycl.fill(4)
+    C_pysycl.fill(0)
+
+    C_np = np.matmul(A_np, B_np)
+    pysycl.linalg.matmul(A_pysycl, B_pysycl, C_pysycl, 32)
+
+    C_pysycl.mem_to_cpu()
+
+    for i in range(M):
+      for j in range(P):
+        self.assertEqual(C_pysycl[i, j], C_np[i, j])
 
 if __name__ == '__main__':
   unittest.main()
